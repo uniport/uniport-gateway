6af43ad3fdb8be95dce84f692a94d9a617e0b734
Florian Bütler <fbuetler@inventage.com>
2021-04-15 18:35:34 +0200

    test redirect middleware

--- server/src/test/java/com/inventage/portal/gateway/proxy/middleware/redirectRegex/RedirectRegexMiddlewareTest.java
+++ server/src/test/java/com/inventage/portal/gateway/proxy/middleware/redirectRegex/RedirectRegexMiddlewareTest.java
@@ -1,83 +1,61 @@
 package com.inventage.portal.gateway.proxy.middleware.redirectRegex;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Router;
 import io.vertx.junit5.Checkpoint;
+import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 
+@ExtendWith(VertxExtension.class)
 public class RedirectRegexMiddlewareTest {
-    Vertx vertx = Vertx.vertx();
 
-    @TestFactory
-    Stream<DynamicTest> testRedirects() {
-        // TODO this should actually fail
-        List<TestInstance> testInstances = Arrays.asList(new TestInstance("simple",
-                "^(/dashboard)$", "$1/", "/dashboard", 301, "/dashboard/"));
-
-        return testInstances.stream()
-                .map(testInstance -> DynamicTest.dynamicTest("Testing " + testInstance.name,
-                        () -> testRedirect(testInstance.regex, testInstance.replacement,
-                                testInstance.requestURI, testInstance.expectedStatusCode,
-                                testInstance.expectedLocation)));
+    static Stream<Arguments> redirectTestData() {
+        // name, regex, replacement, URL, expectedStatusCode, expectedURL
+        return Stream.of(
+                Arguments.of("simple redirection", "^(/dashboard)$", "$1/", "/dashboard",
+                        HttpResponseStatus.FOUND.code(), "/dashboard/"),
+                Arguments.of("URL doesn't match regex", "^/blub/(.*)$", "/$1", "/wont/match",
+                        HttpResponseStatus.OK.code(), null));
     }
 
-    void testRedirect(String regex, String replacement, String requestURI, int expectedStatusCode,
-            String expectedLocation) {
-        VertxTestContext testContext = new VertxTestContext();
+    @ParameterizedTest
+    @MethodSource("redirectTestData")
+    void redirectTest(String name, String regex, String replacement, String URL,
+            int expectedStatusCode, String expectedURL, Vertx vertx, VertxTestContext testCtx) {
 
-        Checkpoint serverStarted = testContext.checkpoint();
-        Checkpoint requestsServed = testContext.checkpoint();
-        Checkpoint responsesReceived = testContext.checkpoint();
+        String failureMsg = String.format("Failure of '%s' test case", name);
+
+        Checkpoint serverStarted = testCtx.checkpoint();
+        Checkpoint requestsServed = testCtx.checkpoint();
+        Checkpoint responsesReceived = testCtx.checkpoint();
 
         RedirectRegexMiddleware redirect = new RedirectRegexMiddleware(regex, replacement);
 
         int port = 8888;
         Router router = Router.router(vertx);
-        router.route().handler(redirect);
+        router.route().handler(redirect).handler(ctx -> ctx.response().end("ok"));
         vertx.createHttpServer().requestHandler(req -> {
             router.handle(req);
             requestsServed.flag();
-        }).listen(port).onComplete(testContext.succeeding(httpServer -> serverStarted.flag()));
+        }).listen(port).onComplete(testCtx.succeeding(httpServer -> serverStarted.flag()));
 
         HttpClient client = vertx.createHttpClient();
 
-        client.request(HttpMethod.GET, port, "localhost", requestURI).compose(req -> req.send())
-                .onSuccess(resp -> {
-                    testContext.succeeding(r -> testContext.verify(() -> {
-                        assertEquals(expectedStatusCode, resp.statusCode());
-                        assertEquals(expectedLocation, resp.headers().get(HttpHeaders.LOCATION));
-                    }));
+        client.request(HttpMethod.GET, port, "localhost", URL).compose(req -> req.send())
+                .onComplete(testCtx.succeeding(resp -> testCtx.verify(() -> {
+                    assertEquals(expectedStatusCode, resp.statusCode(), failureMsg);
+                    assertEquals(expectedURL, resp.headers().get(HttpHeaders.LOCATION), failureMsg);
                     responsesReceived.flag();
-                });
-    }
-
-    private class TestInstance {
-        public String name;
-
-        public String regex;
-        public String replacement;
-
-        public String requestURI;
-        public int expectedStatusCode;
-        public String expectedLocation;
-
-        public TestInstance(String name, String regex, String replacement, String requestURI,
-                int expectedStatusCode, String expectedLocation) {
-            this.name = name;
-            this.regex = regex;
-            this.replacement = replacement;
-            this.requestURI = requestURI;
-            this.expectedStatusCode = expectedStatusCode;
-            this.expectedLocation = expectedLocation;
-        }
+                })));
     }
 }
