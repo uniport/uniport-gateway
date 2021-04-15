d8d5ff1dc88ed95e97741a02dc416caf525d4850
Florian Bütler <fbuetler@inventage.com>
2021-04-15 10:04:17 +0200

    rm trace log stmts

--- server/src/main/java/com/inventage/portal/gateway/proxy/middleware/debug/ShowSessionContentMiddlewareFactory.java
+++ server/src/main/java/com/inventage/portal/gateway/proxy/middleware/debug/ShowSessionContentMiddlewareFactory.java
@@ -3,12 +3,12 @@ package com.inventage.portal.gateway.proxy.middleware.debug;
 import com.inventage.portal.gateway.proxy.config.dynamic.DynamicConfiguration;
 import com.inventage.portal.gateway.proxy.middleware.Middleware;
 import com.inventage.portal.gateway.proxy.middleware.MiddlewareFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ShowSessionContentMiddlewareFactory implements MiddlewareFactory {
 
@@ -17,13 +17,11 @@ public class ShowSessionContentMiddlewareFactory implements MiddlewareFactory {
 
     @Override
     public String provides() {
-        LOGGER.trace("provides");
         return DynamicConfiguration.MIDDLEWARE_SHOW_SESSION_CONTENT;
     }
 
     @Override
     public Future<Middleware> create(Vertx vertx, Router router, JsonObject middlewareConfig) {
-        LOGGER.trace("create");
         LOGGER.debug("create: Created '{}' middleware successfully",
                 DynamicConfiguration.MIDDLEWARE_SHOW_SESSION_CONTENT);
         return Future.succeededFuture(new ShowSessionContentMiddleware());
