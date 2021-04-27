eaa20da89ffaf35ee9269cdff2b5c29b897f522c
Florian Bütler <fbuetler@inventage.com>
2021-04-27 17:43:37 +0200

    run tests in alphabetical order and go back to original find free port version

--- server/src/test/java/com/inventage/portal/gateway/TestUtils.java
+++ server/src/test/java/com/inventage/portal/gateway/TestUtils.java
@@ -12,15 +12,33 @@ import io.vertx.core.json.JsonObject;
 
 public class TestUtils {
     /**
-     * Returns a free port number on localhost, or throws exception if unable to find a free port.
+     * Returns a free port number on localhost.
      *
-     * @return a free port number on localhost, or throws exception  if unable to find a free port
-     * @since 3.0
+     * Heavily inspired from org.eclipse.jdt.launching.SocketUtil (to avoid a dependency to JDT just because of this).
+     * Slightly improved with close() missing in JDT. And throws exception instead of returning -1.
+     *
+     * @return a free port number on localhost
+     * @throws IllegalStateException if unable to find a free port
      */
     public static int findFreePort() {
-        try (ServerSocket socket = new ServerSocket(0)) {
-            return socket.getLocalPort();
+        ServerSocket socket = null;
+        try {
+            socket = new ServerSocket(0);
+            int port = socket.getLocalPort();
+            try {
+                socket.close();
+            } catch (IOException e) {
+                // Ignore IOException on close()
+            }
+            return port;
         } catch (IOException e) {
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
         }
         throw new IllegalStateException("Could not find a free TCP/IP port to start embedded Jetty HTTP Server on");
     }
