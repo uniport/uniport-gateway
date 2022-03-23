b634061d57d9999d70834f5cb69e45985a607f0b
Kevin Tang <kevin.tang@inventage.com>
2022-03-23 15:52:01 +0100

    PORTAL-654: - (Ongoing) Added more tests and useful debugging statements. Local test on IPS organisation proxy (based on portal gateway) all pass successfully.

--- server/src/main/java/com/inventage/portal/gateway/proxy/middleware/bearerOnly/customClaimsChecker/JWTClaimOperator.java
+++ server/src/main/java/com/inventage/portal/gateway/proxy/middleware/bearerOnly/customClaimsChecker/JWTClaimOperator.java
@@ -1,10 +1,13 @@
 package com.inventage.portal.gateway.proxy.middleware.bearerOnly.customClaimsChecker;
 
+import com.inventage.portal.gateway.proxy.config.dynamic.DynamicConfiguration;
+
 public enum JWTClaimOperator {
-    EQUALS("equals"), CONTAINS("contains");
+    EQUALS(DynamicConfiguration.MIDDLEWARE_BEARER_ONLY_CLAIM_OPERATOR_EQUALS),
+    CONTAINS(DynamicConfiguration.MIDDLEWARE_BEARER_ONLY_CLAIM_OPERATOR_CONTAINS);
 
     private final String name;
-    private JWTClaimOperator(String name){
+    JWTClaimOperator(String name){
         this.name = name;
     }
 
@@ -12,5 +15,6 @@ public enum JWTClaimOperator {
     public String toString() {
         return this.name;
     }
+
 
 }
