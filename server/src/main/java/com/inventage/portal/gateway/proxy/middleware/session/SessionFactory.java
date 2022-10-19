2e7e26aaa50d08eb6631f13c04cfb946fc3efe08
Kevin Tang <kevin.tang@inventage.com>
2022-10-19 14:53:23 +0200

    PORTAL-949: Fix bug in sessionfactory.

--- server/src/main/java/com/inventage/portal/gateway/proxy/middleware/session/SessionFactory.java
+++ server/src/main/java/com/inventage/portal/gateway/proxy/middleware/session/SessionFactory.java
@@ -18,13 +18,13 @@ public class SessionFactory implements MiddlewareFactory {
     @Override
     public Future<Middleware> create(Vertx vertx, Router router, JsonObject middlewareConfig) {
         JsonObject cookie = middlewareConfig.getJsonObject(DynamicConfiguration.MIDDLEWARE_SESSION_COOKIE);
-        String cookieName = cookie.getString(DynamicConfiguration.MIDDLEWARE_RESPONSE_SESSION_COOKIE_REMOVAL_NAME);
-        boolean cookieHttpOnly = cookie.getBoolean(DynamicConfiguration.MIDDLEWARE_SESSION_COOKIE_HTTP_ONLY);
-        boolean cookieSecure = cookie.getBoolean(DynamicConfiguration.MIDDLEWARE_SESSION_COOKIE_SECURE);
-        String cookieSameSite = cookie.getString(DynamicConfiguration.MIDDLEWARE_SESSION_COOKIE_SAME_SITE);
-        long sessionIdleTimeoutInMinutes = middlewareConfig.getInteger(DynamicConfiguration.MIDDLEWARE_SESSION_IDLE_TIMEOUT_IN_MINUTES);
-        int sessionIdMinLength = middlewareConfig.getInteger(DynamicConfiguration.MIDDLEWARE_SESSION_ID_MIN_LENGTH);
-        boolean nagHttps = middlewareConfig.getBoolean(DynamicConfiguration.MIDDLEWARE_SESSION_NAG_HTTPS);
+        String cookieName = (cookie == null) ? null : cookie.getString(DynamicConfiguration.MIDDLEWARE_RESPONSE_SESSION_COOKIE_REMOVAL_NAME);
+        Boolean cookieHttpOnly = (cookie == null) ? null : cookie.getBoolean(DynamicConfiguration.MIDDLEWARE_SESSION_COOKIE_HTTP_ONLY);
+        Boolean cookieSecure = (cookie == null) ? null : cookie.getBoolean(DynamicConfiguration.MIDDLEWARE_SESSION_COOKIE_SECURE);
+        String cookieSameSite = (cookie == null) ? null : cookie.getString(DynamicConfiguration.MIDDLEWARE_SESSION_COOKIE_SAME_SITE);
+        Long sessionIdleTimeoutInMinutes = middlewareConfig.getLong(DynamicConfiguration.MIDDLEWARE_SESSION_IDLE_TIMEOUT_IN_MINUTES);
+        Integer sessionIdMinLength = middlewareConfig.getInteger(DynamicConfiguration.MIDDLEWARE_SESSION_ID_MIN_LENGTH);
+        Boolean nagHttps = middlewareConfig.getBoolean(DynamicConfiguration.MIDDLEWARE_SESSION_NAG_HTTPS);
 
         LOGGER.info("Created '{}' middleware successfully",
                 DynamicConfiguration.MIDDLEWARE_SESSION);
