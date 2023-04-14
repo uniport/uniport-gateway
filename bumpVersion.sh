c71e870ee8ffd0f55033aee46778aec052323671
Tobias Gmuender <tobias.gmuender@inventage.com>
2023-04-14 14:28:38 +0200

    Utility script to set versions in pom files

---
+++ bumpVersion.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function printUsage {
+    cat <<EOF
+    Provide single parameter for the new version, e.g. 1.2.3
+
+    Usage:
+            $0 1.2.3
+            or
+            $0.1.2.3-SNAPSHOT
+EOF
+}
+
+if [[ "$#" -eq 0 ]]
+ then
+    printUsage
+    exit
+fi
+
+readonly newVersion=$1
+
+mvn versions:set \
+    -DgenerateBackupPoms=false \
+    -DnewVersion=${newVersion}
