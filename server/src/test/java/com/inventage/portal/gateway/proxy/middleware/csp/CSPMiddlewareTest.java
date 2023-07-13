1c6b28f5013ec72acc2d6c5fe000d02e59a5b5e5
Florian Bütler <fbuetler@inventage.com>
2023-07-13 15:05:33 +0200

    fix compiler warning

--- server/src/test/java/com/inventage/portal/gateway/proxy/middleware/csp/CSPMiddlewareTest.java
+++ server/src/test/java/com/inventage/portal/gateway/proxy/middleware/csp/CSPMiddlewareTest.java
@@ -26,23 +26,23 @@ class CSPMiddlewareTest {
     @Test
     void checkForCspInHTTPHeader(Vertx vertx, VertxTestContext testCtx) {
         //given
-        final String default_src = "default-src";
-        final String default_src_value = "self";
+        final String defaultSrc = "default-src";
+        final String defaultSrcValue = "self";
 
         final MiddlewareServer gateway = portalGateway(vertx, testCtx)
-            .withCspMiddleware(JsonArray.of(createDirective(default_src, default_src_value)), false)
+            .withCspMiddleware(JsonArray.of(createDirective(defaultSrc, defaultSrcValue)), false)
             .build().start();
 
         //when
         gateway.incomingRequest(HttpMethod.GET, "/", (httpClientResponse -> {
             //then
             final MultiMap headers = httpClientResponse.headers();
-            final String csp_values = headers.get(CONTENT_SECURITY_POLICY);
+            final String cspValues = headers.get(CONTENT_SECURITY_POLICY);
 
-            assertNotNull(csp_values,
+            assertNotNull(cspValues,
                 String.format("'%s' is NOT contained in http response header!",
                     CONTENT_SECURITY_POLICY));
-            assertTrue(csp_values.contains(default_src) && csp_values.contains(default_src_value),
+            assertTrue(cspValues.contains(defaultSrc) && cspValues.contains(defaultSrcValue),
                 " Csp directive and value NOT contained in http response header!");
             testCtx.completeNow();
         }));
@@ -51,34 +51,34 @@ class CSPMiddlewareTest {
     @Test
     void checkReportOnlyInHTTPHeader(Vertx vertx, VertxTestContext testCtx) {
         //given
-        final String default_src = "default-src";
-        final String default_src_value = "self";
+        final String defaultSrc = "default-src";
+        final String defaultSrcValue = "self";
 
-        final String report_to = "report-to";
-        final String report_to_value = "www.example.com";
+        final String reportTo = "report-to";
+        final String reportToValue = "www.example.com";
 
         final MiddlewareServer gateway = portalGateway(vertx, testCtx)
-            .withCspMiddleware(JsonArray.of(createDirective(default_src, default_src_value),
-                createDirective(report_to, report_to_value)), true)
+            .withCspMiddleware(JsonArray.of(createDirective(defaultSrc, defaultSrcValue),
+                createDirective(reportTo, reportToValue)), true)
             .build().start();
 
         //when
         gateway.incomingRequest(HttpMethod.GET, "/", (httpClientResponse -> {
             //then
             final MultiMap headers = httpClientResponse.headers();
-            final String csp_values_report_only = headers.get(CONTENT_SECURITY_POLICY_REPORT_ONLY);
-            final String csp_values = headers.get(CONTENT_SECURITY_POLICY);
+            final String cspValuesReportOnly = headers.get(CONTENT_SECURITY_POLICY_REPORT_ONLY);
+            final String cpsValues = headers.get(CONTENT_SECURITY_POLICY);
 
-            assertNotNull(csp_values_report_only,
+            assertNotNull(cspValuesReportOnly,
                 String.format("'%s' is NOT contained in http response header!",
                     CONTENT_SECURITY_POLICY));
             assertTrue(
-                csp_values_report_only.contains(default_src)
-                    && csp_values_report_only.contains(default_src_value)
-                    && csp_values_report_only.contains(report_to_value),
+                cspValuesReportOnly.contains(defaultSrc)
+                    && cspValuesReportOnly.contains(defaultSrcValue)
+                    && cspValuesReportOnly.contains(reportToValue),
                 String.format("Csp report only directives should be contained in the header entry: %s!",
                     CONTENT_SECURITY_POLICY_REPORT_ONLY));
-            assertNull(csp_values, " CSP header should not be set");
+            assertNull(cpsValues, " CSP header should not be set");
 
             testCtx.completeNow();
         }));
@@ -87,29 +87,29 @@ class CSPMiddlewareTest {
     @Test
     void chainingTwoCSPMiddlewaresDifferentDirectives(Vertx vertx, VertxTestContext testCtx) {
         //given
-        final String style_directive = "style-src";
-        final String style_value = "self";
+        final String styleDirective = "style-src";
+        final String styleValue = "self";
 
-        final String media_directive = "media-src";
-        final String media_value = "self";
+        final String mediaDirective = "media-src";
+        final String mediaValue = "self";
 
         final MiddlewareServer gateway = portalGateway(vertx, testCtx)
-            .withCspMiddleware(JsonArray.of(createDirective(media_directive, media_value)), false)
-            .withCspMiddleware(JsonArray.of(createDirective(style_directive, style_value)), false)
+            .withCspMiddleware(JsonArray.of(createDirective(mediaDirective, mediaValue)), false)
+            .withCspMiddleware(JsonArray.of(createDirective(styleDirective, styleValue)), false)
             .build().start();
 
         //when
         gateway.incomingRequest(HttpMethod.GET, "/", (httpClientResponse -> {
             //then
             final MultiMap headers = httpClientResponse.headers();
-            final String csp_values = headers.get(CONTENT_SECURITY_POLICY);
+            final String cspValues = headers.get(CONTENT_SECURITY_POLICY);
 
-            assertNotNull(csp_values,
+            assertNotNull(cspValues,
                 String.format("'%s' is NOT contained in http response header!",
                     CONTENT_SECURITY_POLICY));
-            assertTrue(csp_values.contains(style_directive) && csp_values.contains(style_value),
+            assertTrue(cspValues.contains(styleDirective) && cspValues.contains(styleValue),
                 " Csp directive and value NOT contained in http response header!");
-            assertTrue(csp_values.contains(media_directive) && csp_values.contains(media_value),
+            assertTrue(cspValues.contains(mediaDirective) && cspValues.contains(mediaValue),
                 " Csp directive and value NOT contained in http response header!");
             testCtx.completeNow();
         }));
@@ -118,30 +118,30 @@ class CSPMiddlewareTest {
     @Test
     void chainingTwoCSPMiddlewaresWithSameDirective(Vertx vertx, VertxTestContext testCtx) {
         //given
-        final String style_directive = "style-src";
-        final String style_value = "self";
-        final String different_style_value = "https://fonts.googleapis.com https://fonts.gstatic.com https://cdn.jsdelivr.net";
+        final String styleDirective = "style-src";
+        final String styleValue = "self";
+        final String differentStyleValue = "https://fonts.googleapis.com https://fonts.gstatic.com https://cdn.jsdelivr.net";
 
         final MiddlewareServer gateway = portalGateway(vertx, testCtx)
-            .withCspMiddleware(JsonArray.of(createDirective(style_directive, different_style_value)), false)
-            .withCspMiddleware(JsonArray.of(createDirective(style_directive, style_value)), false)
+            .withCspMiddleware(JsonArray.of(createDirective(styleDirective, differentStyleValue)), false)
+            .withCspMiddleware(JsonArray.of(createDirective(styleDirective, styleValue)), false)
             .build().start();
 
         //when
         gateway.incomingRequest(HttpMethod.GET, "/", (httpClientResponse -> {
             //then
             final MultiMap headers = httpClientResponse.headers();
-            final String csp_values = headers.get(CONTENT_SECURITY_POLICY);
+            final String cspValues = headers.get(CONTENT_SECURITY_POLICY);
 
-            assertNotNull(csp_values,
+            assertNotNull(cspValues,
                 String.format("'%s' is NOT contained in http response header!",
                     CONTENT_SECURITY_POLICY));
-            assertTrue(csp_values.contains(style_directive) && csp_values.contains(style_value),
+            assertTrue(cspValues.contains(styleDirective) && cspValues.contains(styleValue),
                 " Csp directive and value NOT contained in http response header!");
 
-            for (String expected_value : different_style_value.split(" ")) {
-                assertTrue(csp_values.contains(expected_value),
-                    " Expected " + expected_value + " in csp header");
+            for (String expectedValue : differentStyleValue.split(" ")) {
+                assertTrue(cspValues.contains(expectedValue),
+                    " Expected " + expectedValue + " in csp header");
             }
 
             testCtx.completeNow();
@@ -150,6 +150,6 @@ class CSPMiddlewareTest {
 
     private JsonObject createDirective(String directive, String... values) {
         return JsonObject.of(DynamicConfiguration.MIDDLEWARE_CSP_DIRECTIVE_NAME, directive,
-            DynamicConfiguration.MIDDLEWARE_CSP_DIRECTIVE_VALUES, JsonArray.of(values));
+            DynamicConfiguration.MIDDLEWARE_CSP_DIRECTIVE_VALUES, JsonArray.of((Object) values));
     }
 }
