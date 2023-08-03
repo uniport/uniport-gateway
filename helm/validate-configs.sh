45ca380ac270caffb62b0000260b5363a2b57ef6
Tobias Gmuender <tobias.gmuender@inventage.com>
2023-08-03 14:49:34 +0200

    Invert module dependencies when validating config files
    This commit moves the schema validation of the config files from the config-schemas-module to the helm-module. Also moves the validation step during the verify phase (was: package).

---
+++ helm/validate-configs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This script checks if the json files used in the helm chart is valid according to their corresponding schemas.
+
+set -Eeo pipefail
+
+declare -r NODE_IMAGE=node:18-alpine
+
+if [ -n "$MINIKUBE_ACTIVE_DOCKERD" ]
+then
+    echo "\$MINIKUBE_ACTIVE_DOCKERD is set, skipping validation…"
+    exit
+fi
+
+function validateWithDynamicSchema() {
+  docker run --rm \
+    -e NPM_CONFIG_UPDATE_NOTIFIER=false \
+    -e NPM_CONFIG_YES=true \
+    -v "${PWD}":/tmp \
+    $NODE_IMAGE \
+    npx ajv-cli validate --allow-union-types -s /tmp/target/schema/portalGatewayDynamicSchema.json -d "/tmp/target/portal-gateway/proxy-config.*/dynamic-config/**/*.json"
+}
+
+function validateWithStaticSchema() {
+  docker run --rm \
+    -e NPM_CONFIG_UPDATE_NOTIFIER=false \
+    -e NPM_CONFIG_YES=true \
+    -v "${PWD}":/tmp \
+    $NODE_IMAGE \
+    npx ajv-cli validate --allow-union-types -s /tmp/target/schema/portalGatewayStaticSchema.json -d "/tmp/target/portal-gateway/proxy-config.*/*.json"
+}
+
+validateWithDynamicSchema
+validateWithStaticSchema
