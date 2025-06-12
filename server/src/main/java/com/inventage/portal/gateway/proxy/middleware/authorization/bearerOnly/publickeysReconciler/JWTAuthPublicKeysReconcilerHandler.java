ed994f1d125f243416de9910594afac9077071a7
Florian Bütler <fbuetler@inventage.com>
2025-06-12 14:12:10 +0200

    PORTAL-2315: refactor to remove the promise usage in JWTAuthPublicKeysReconcilerHandler.fetchJWKsFromDiscoveryURL

--- server/src/main/java/com/inventage/portal/gateway/proxy/middleware/authorization/bearerOnly/publickeysReconciler/JWTAuthPublicKeysReconcilerHandler.java
+++ server/src/main/java/com/inventage/portal/gateway/proxy/middleware/authorization/bearerOnly/publickeysReconciler/JWTAuthPublicKeysReconcilerHandler.java
@@ -4,10 +4,7 @@ import com.inventage.portal.gateway.proxy.middleware.authorization.JWKAccessible
 import com.inventage.portal.gateway.proxy.middleware.authorization.PublicKeyOptions;
 import com.inventage.portal.gateway.proxy.middleware.authorization.bearerOnly.customClaimsChecker.JWTAuthAdditionalClaimsOptions;
 import com.inventage.portal.gateway.proxy.middleware.authorization.bearerOnly.customIssuerChecker.JWTAuthMultipleIssuersOptions;
-import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
-import io.vertx.core.Handler;
-import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
@@ -78,7 +75,7 @@ public interface JWTAuthPublicKeysReconcilerHandler extends JWKAccessibleAuthHan
         publicKeys.forEach(pk -> {
             final String publicKey = pk.getKey();
 
-            if (isURL(publicKey)) {
+            if (isURI(publicKey)) {
                 LOGGER.info("Public key provided by URL. Fetching JWKs...");
                 futures.add(
                     fetchJWKsFromDiscoveryURL(vertx, publicKey)
@@ -88,7 +85,7 @@ public interface JWTAuthPublicKeysReconcilerHandler extends JWKAccessibleAuthHan
                 authOpts.addPubSecKey(
                     new PubSecKeyOptions()
                         .setAlgorithm(pk.getAlgorithm())
-                        .setBuffer(publickeyToPEM(publicKey)));
+                        .setBuffer(publicKeyToPEM(publicKey)));
             }
         });
 
@@ -99,33 +96,23 @@ public interface JWTAuthPublicKeysReconcilerHandler extends JWKAccessibleAuthHan
             });
     }
 
-    private static boolean isURL(String publicKey) {
-        try {
-            new URI(publicKey);
-        } catch (URISyntaxException e) {
-            LOGGER.debug("URI is malformed, hence it is has to be a raw public key: '{}'", e.getMessage());
-            return false;
-        }
-        return true;
-    }
-
+    /**
+     * Fetches the JWKs via the OIDC discovery page as specified by OpenID Connect 1.0
+     * See https://openid.net/specs/openid-connect-discovery-1_0-final.html#ProviderConfig
+     * 
+     * As there are multiple requests issued, the scheme/host/port of the provided URI are parsed.
+     * Additionally, the .well-known openid configuration path is appended, if not yet present.
+     * 
+     * @param vertx
+     * @param rawRealmBaseURL
+     * @return
+     */
     private static Future<List<JsonObject>> fetchJWKsFromDiscoveryURL(Vertx vertx, String rawRealmBaseURL) {
-        final Promise<List<JsonObject>> promise = Promise.promise();
-        fetchJWKsFromDiscoveryURL(vertx, rawRealmBaseURL, promise);
-        return promise.future();
-    }
-
-    private static void fetchJWKsFromDiscoveryURL(
-        Vertx vertx, String rawRealmBaseURL,
-        Handler<AsyncResult<List<JsonObject>>> handler
-    ) {
-        final URL parsedRealmBaseURL;
-        try {
-            parsedRealmBaseURL = new URL(rawRealmBaseURL);
-        } catch (MalformedURLException e) {
-            LOGGER.warn("Malformed discovery URL '{}'", rawRealmBaseURL);
-            handler.handle(Future.failedFuture(e));
-            return;
+        final URL parsedRealmBaseURL = parseURI(rawRealmBaseURL);
+        if (parsedRealmBaseURL == null) {
+            final String errMsg = String.format("Malformed discovery URL '%s'", rawRealmBaseURL);
+            LOGGER.warn(errMsg);
+            return Future.failedFuture(errMsg);
         }
 
         final String iamProtocol = parsedRealmBaseURL.getProtocol();
@@ -159,86 +146,103 @@ public interface JWTAuthPublicKeysReconcilerHandler extends JWKAccessibleAuthHan
         final int iamPort = port;
         final String iamDiscoveryPath = path;
         LOGGER.debug("Fetching jwks_uri from URL '{}://{}:{}{}'", iamProtocol, iamHost, iamPort, iamDiscoveryPath);
-        WebClient.create(vertx).get(iamPort, iamHost, iamDiscoveryPath).as(BodyCodec.jsonObject()).send()
-            .onSuccess(fetchJWKsFromJWKsURL(vertx, iamHost, iamPort, handler))
-            .onFailure(err -> {
-                LOGGER.info("Failed to complete discovery from URL '{}://{}:{}{}'",
-                    iamProtocol, iamHost, iamPort, iamDiscoveryPath);
-                handler.handle(Future.failedFuture(err));
-            });
+        return WebClient.create(vertx)
+            .get(iamPort, iamHost, iamDiscoveryPath)
+            .as(BodyCodec.jsonObject())
+            .send()
+            .compose(discoveryResp -> fetchJWKsFromJWKsURL(vertx, iamHost, iamPort, discoveryResp))
+            .onFailure(err -> LOGGER.info("Failed to complete discovery from URL '{}://{}:{}{}: {}'", iamProtocol, iamHost, iamPort, iamDiscoveryPath, err));
     }
 
-    private static Handler<HttpResponse<JsonObject>> fetchJWKsFromJWKsURL(
-        Vertx vertx, String iamHost,
-        int iamPort, Handler<AsyncResult<List<JsonObject>>> handler
-    ) {
-        return discoveryResp -> {
-            final String rawJWKsURI = discoveryResp.body().getString(JWKS_URI_KEY);
-            if (rawJWKsURI == null || rawJWKsURI.length() == 0) {
-                final String errMsg = "No JWK URI found";
-                LOGGER.warn(errMsg);
-                handler.handle(Future.failedFuture(errMsg));
-                return;
-            }
+    /**
+     * The discovery response is JSON and is specified by the OpenID Connect 1.0
+     * See https://openid.net/specs/openid-connect-discovery-1_0-final.html#ProviderMetadata
+     * 
+     * @param vertx
+     * @param iamHost
+     * @param iamPort
+     * @param discoveryResp
+     *            discovery response as JSON
+     * @return
+     */
+    private static Future<List<JsonObject>> fetchJWKsFromJWKsURL(Vertx vertx, String iamHost, int iamPort, HttpResponse<JsonObject> discoveryResp) {
+        final String rawJWKsURI = discoveryResp.body().getString(JWKS_URI_KEY);
+        if (rawJWKsURI == null || rawJWKsURI.length() == 0) {
+            final String errMsg = "No JWK URI found";
+            LOGGER.warn(errMsg);
+            return Future.failedFuture(errMsg);
+        }
 
-            final URL parsedJWKsURL;
-            try {
-                parsedJWKsURL = new URL(rawJWKsURI);
-            } catch (MalformedURLException e) {
-                LOGGER.warn("Malformed JWKs URL '{}'", rawJWKsURI);
-                handler.handle(Future.failedFuture(e));
-                return;
-            }
+        final URL parsedJWKsURL = parseURI(rawJWKsURI);
+        if (parsedJWKsURL == null) {
+            final String errMsg = String.format("Malformed JWKs URL '%s'", rawJWKsURI);
+            LOGGER.warn(errMsg);
+            return Future.failedFuture(errMsg);
+        }
 
-            final String iamJWKsPath = parsedJWKsURL.getPath();
-            if (iamJWKsPath == null) {
-                final String errMsg = "Failed to discover JWKs URI";
-                LOGGER.warn(errMsg);
-                handler.handle(Future.failedFuture(errMsg));
-                return;
-            }
+        final String iamJWKsPath = parsedJWKsURL.getPath();
+        if (iamJWKsPath == null) {
+            final String errMsg = "Failed to discover JWKs URI";
+            LOGGER.warn(errMsg);
+            return Future.failedFuture(errMsg);
+        }
 
-            LOGGER.debug("Fetching JWKS from URL '{}'", rawJWKsURI);
-            WebClient.create(vertx).get(iamPort, iamHost, iamJWKsPath).as(BodyCodec.jsonObject()).send()
-                .onSuccess(parseJWKs(handler))
-                .onFailure(err -> {
-                    LOGGER.info("Failed to complete load JWK from URL '{}'", rawJWKsURI);
-                    handler.handle(Future.failedFuture(err));
-                });
-        };
+        LOGGER.debug("Fetching JWKS from URL '{}'", rawJWKsURI);
+        return WebClient.create(vertx)
+            .get(iamPort, iamHost, iamJWKsPath)
+            .as(BodyCodec.jsonObject())
+            .send()
+            .compose(jwksResponse -> parseJWKs(jwksResponse))
+            .onFailure(err -> LOGGER.info("Failed to complete load JWK from URL '{}'", rawJWKsURI));
     }
 
-    private static Handler<HttpResponse<JsonObject>> parseJWKs(
-        Handler<AsyncResult<List<JsonObject>>> handler
-    ) {
-        return JWKsResp -> {
-            LOGGER.debug("Received JWKS");
-            final JsonArray keys = JWKsResp.body().getJsonArray(JWK_KEYS_KEY);
-            if (keys == null || keys.size() == 0) {
-                final String errMsg = "No JWK found";
-                LOGGER.warn(errMsg);
-                handler.handle(Future.failedFuture(errMsg));
-                return;
-            }
+    /**
+     * The JWK response is JSON and is specified by RFC7517
+     * See https://datatracker.ietf.org/doc/html/rfc7517#section-5.1
+     * 
+     * @param jwksResponse
+     *            the JWK response as JSON
+     * @return
+     */
+    private static Future<List<JsonObject>> parseJWKs(HttpResponse<JsonObject> jwksResponse) {
+        LOGGER.debug("Received JWKS");
+        final JsonArray keys = jwksResponse.body().getJsonArray(JWK_KEYS_KEY);
+        if (keys == null || keys.size() == 0) {
+            final String errMsg = "No JWK found";
+            LOGGER.warn(errMsg);
+            return Future.failedFuture(errMsg);
+        }
 
-            final List<JsonObject> jwks = new ArrayList<JsonObject>();
-            for (int i = 0; i < keys.size(); i++) {
-                final JsonObject json = keys.getJsonObject(i);
-                LOGGER.debug("Fetched JWK with kid '{}'", json.getString(JWK_KID));
-                jwks.add(json);
-            }
+        final List<JsonObject> jwks = new ArrayList<JsonObject>();
+        for (int i = 0; i < keys.size(); i++) {
+            final JsonObject json = keys.getJsonObject(i);
+            LOGGER.debug("Fetched JWK with kid '{}'", json.getString(JWK_KID));
+            jwks.add(json);
+        }
 
-            LOGGER.debug("Successfully fetched {} JWKS from URL", keys.size());
-            handler.handle(Future.succeededFuture(jwks));
-        };
+        LOGGER.debug("Successfully fetched {} JWKS from URL", keys.size());
+        return Future.succeededFuture(jwks);
     }
 
-    private static String publickeyToPEM(String publicKey) {
+    private static String publicKeyToPEM(String publicKey) {
         return String.join(
             "\n",
             "-----BEGIN PUBLIC KEY-----",
             publicKey,
             "-----END PUBLIC KEY-----");
+    }
+
+    private static boolean isURI(String publicKey) {
+        return parseURI(publicKey) != null;
+    }
+
+    private static URL parseURI(String publicKey) {
+        try {
+            return new URI(publicKey).toURL();
+        } catch (URISyntaxException | MalformedURLException | IllegalArgumentException e) {
+            LOGGER.debug("URI is malformed, hence it is has to be a raw public key: '{}'", e.getMessage());
+            return null;
+        }
     }
 
 }
