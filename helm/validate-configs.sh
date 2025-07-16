cee60fe05f28da61ad4bf4bb14d89fd540421663
Florian Bütler <fbuetler@inventage.com>
2025-07-16 17:22:06 +0200

    PORTAL-2143: replace portal-gateway with uniport-gateway in various config

--- helm/validate-configs.sh
+++ helm/validate-configs.sh
@@ -5,28 +5,27 @@ set -Eeo pipefail
 
 declare -r NODE_IMAGE=uniportcr.artifacts.inventage.com/node:20-alpine
 
-if [ -n "$MINIKUBE_ACTIVE_DOCKERD" ]
-then
+if [ -n "$MINIKUBE_ACTIVE_DOCKERD" ]; then
     echo "\$MINIKUBE_ACTIVE_DOCKERD is set, skipping validation…"
     exit
 fi
 
 function validateWithDynamicSchema() {
-  docker run --rm \
-    -e NPM_CONFIG_UPDATE_NOTIFIER=false \
-    -e NPM_CONFIG_YES=true \
-    -v "${PWD}":/tmp \
-    $NODE_IMAGE \
-    npx ajv-cli validate --allow-union-types -s /tmp/target/schema/portalGatewayDynamicSchema.json -d "/tmp/target/portal-gateway/proxy-config.*/dynamic-config/**/*.json"
+    docker run --rm \
+        -e NPM_CONFIG_UPDATE_NOTIFIER=false \
+        -e NPM_CONFIG_YES=true \
+        -v "${PWD}":/tmp \
+        $NODE_IMAGE \
+        npx ajv-cli validate --allow-union-types -s /tmp/target/schema/portalGatewayDynamicSchema.json -d "/tmp/target/uniport-gateway/proxy-config.*/dynamic-config/**/*.json"
 }
 
 function validateWithStaticSchema() {
-  docker run --rm \
-    -e NPM_CONFIG_UPDATE_NOTIFIER=false \
-    -e NPM_CONFIG_YES=true \
-    -v "${PWD}":/tmp \
-    $NODE_IMAGE \
-    npx ajv-cli validate --allow-union-types -s /tmp/target/schema/portalGatewayStaticSchema.json -d "/tmp/target/portal-gateway/proxy-config.*/*.json"
+    docker run --rm \
+        -e NPM_CONFIG_UPDATE_NOTIFIER=false \
+        -e NPM_CONFIG_YES=true \
+        -v "${PWD}":/tmp \
+        $NODE_IMAGE \
+        npx ajv-cli validate --allow-union-types -s /tmp/target/schema/portalGatewayStaticSchema.json -d "/tmp/target/uniport-gateway/proxy-config.*/*.json"
 }
 
 validateWithDynamicSchema
